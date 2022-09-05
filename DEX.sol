1370cfb27d35851015fdb8384f17aea9eaf1981a
pradip-tcs <100670828+pradip9821-tcs@users.noreply.github.com>
2022-09-05 17:18:37 +0530

    Initial commit

---
+++ DEX.sol
@@ -0,0 +1,36 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.0;
+
+import "./Token.sol";
+
+contract DEX {
+
+    event Bought(uint256 amount);
+    event Sold(uint256 amount);
+
+    Token public token;
+
+    constructor(uint256 _totalSupply) {
+        token = new Token(_totalSupply);
+    }
+
+    function buy() payable public {
+        uint256 amountTobuy = msg.value;
+        uint256 dexBalance = token.balanceOf(address(this));
+        require(amountTobuy > 0, "You need to send some ether");
+        require(amountTobuy <= dexBalance, "Not enough tokens in the reserve");
+        token.transfer(msg.sender, amountTobuy);
+        emit Bought(amountTobuy);
+    }
+
+    function sell(uint256 amount) public {
+        require(amount > 0, "You need to sell at least some tokens");
+        uint256 allowance = token.allowance(msg.sender, address(this));
+        require(allowance >= amount, "Check the token allowance");
+        token.transferFrom(msg.sender, address(this), amount);
+        payable(msg.sender).transfer(amount);
+        emit Sold(amount);
+    }
+
+}
---
+++ README.md
@@ -0,0 +1,1 @@
+# DEX-Using-ERC20
